b128b1b7d720d8b8f8b8372b3b2e07314c9fd089
Andreas Koestler <andreas.koestler@gmail.com>
2011-12-18 18:17:08 +1030

    Initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-0MQ "1.0.0-SNAPSHOT"
+  :description "Clojure client for zeromq"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.zmq/jzmq "2.0.6-SNAPSHOT"]]
+  :native-path "/usr/local/lib")
---
+++ test/clj_zeromq/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-zeromq.test.core
+  (:use [clj-zeromq.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
